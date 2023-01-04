14778e4cf8d7c7f2a7ad902ec7d4b8edd4517470
GGG <GGG@DESKTOP-8N3GG4O>
2023-01-05 08:34:42 +0900

    mypageDialog 클래스 생성

---
+++ src/mypage/MypageDialog.java
@@ -0,0 +1,47 @@
+package mypage;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class MypageDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			MypageDialog dialog = new MypageDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public MypageDialog() {
+		setBounds(100, 100, 498, 433);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(Color.WHITE);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("마이페이지");
+		lblNewLabel.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+		lblNewLabel.setBounds(156, 36, 166, 61);
+		contentPanel.add(lblNewLabel);
+	}
+}
