58c80d210c4780da267b3045adf8024b5ea84c4f
김경연 <115720940+yeoni96@users.noreply.github.com>
2023-01-06 00:00:15 +0900

    commit
    GameFrame 엔터, 마우스클릭으로 이미지 바뀔 수 있도록 구현

--- src/gamemode/GameFrame.java
+++ src/gamemode/GameFrame.java
@@ -1,25 +1,56 @@
 package gamemode;
 
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import imagemanagement.ImageManager;
 
 public class GameFrame extends JFrame {
+	JLabel label;
+	int currentImageIndex = 0;
 	
+
 	public GameFrame() {
 		GameModeServiceImpl gms = new GameModeServiceImpl(new GameModeServiceToolImpl());
+		ImageManager im = new ImageManager();
 		
-		Toolkit kit = Toolkit.getDefaultToolkit();
-		ClassLoader classLoader = getClass().getClassLoader();
-		Image image = kit.getImage(classLoader.getResource("대화1.jpg"));
-		
-		
-		
+		label = new JLabel(new ImageIcon(im.getImages().get(currentImageIndex)));
+		getContentPane().add(label, BorderLayout.CENTER);
+
 		setSize(1280, 845);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+		label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				currentImageIndex = (currentImageIndex + 1) % im.getImages().size();
+				label.setIcon(new ImageIcon(im.getImages().get(currentImageIndex)));
+			}
+		});
+
+		
+		label.setFocusable(true);
+		label.addKeyListener(new KeyAdapter() {
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					currentImageIndex = (currentImageIndex + 1) % im.getImages().size();
+					label.setIcon(new ImageIcon(im.getImages().get(currentImageIndex)));
+				}
+			}
+		});
 	}
+
 	public static void main(String[] args) {
 		GameFrame gf = new GameFrame();
 		gf.setVisible(true);
--- src/gamemode/GameModeService.java
+++ src/gamemode/GameModeService.java
@@ -1,5 +1,5 @@
 package gamemode;
 
 public interface GameModeService {
-
+	
 }
